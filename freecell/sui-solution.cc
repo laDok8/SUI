aaf171799ef16f536e8b7a1e00888f3a90a27252
Peter Zdravecký <pzdravec@redhat.com>
2022-09-27 09:31:27 +0200

    BFS with memory checking

--- freecell/sui-solution.cc
+++ freecell/sui-solution.cc
@@ -6,15 +6,25 @@
 #include <stack>
 #include <algorithm>
 
-std::vector<SearchAction> reconstructPath(const std::map<std::shared_ptr<SearchState>, std::pair<std::shared_ptr<SearchState>, SearchAction>>& parent,
-                                          const std::shared_ptr<SearchState>& current)
+#define SEARCH_STATE_SIZE sizeof(SearchState)
+#define SEARCH_ACTION_SIZE sizeof(SearchAction)
+#define SEARCH_STATE_PTR_SIZE sizeof(SearchState) + sizeof(std::shared_ptr<SearchState>)
+#define NODE_SIZE sizeof(std::pair<std::shared_ptr<SearchState>, SearchAction>)
+
+#define CHECK_MEMORY_LIMIT(mem_limit_, alloc_size)  \
+    if (getCurrentRSS() + alloc_size >= mem_limit_) \
+        return {};
+
+std::vector<SearchAction> reconstructPath(const std::map<std::shared_ptr<SearchState>, std::pair<std::shared_ptr<SearchState>, SearchAction>> &nodes,
+                                          const std::shared_ptr<SearchState> &current, const size_t mem_limit_)
 {
+
     std::vector<SearchAction> solution;
     std::shared_ptr<SearchState> state = current;
-    while (parent.find(state) != parent.end())
+    while (nodes.find(state) != nodes.end())
     {
-        solution.push_back(parent.at(state).second);
-        state = parent.at(state).first;
+        solution.push_back(nodes.at(state).second);
+        state = nodes.at(state).first;
     }
     std::reverse(solution.begin(), solution.end());
     return solution;
@@ -22,9 +32,10 @@ std::vector<SearchAction> reconstructPath(const std::map<std::shared_ptr<SearchS
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state)
 {
+
     std::queue<std::shared_ptr<SearchState>> open;
     std::set<SearchState> closed;
-    std::map<std::shared_ptr<SearchState>, std::pair<std::shared_ptr<SearchState>, SearchAction>> parent;
+    std::map<std::shared_ptr<SearchState>, std::pair<std::shared_ptr<SearchState>, SearchAction>> nodes;
 
     if (init_state.isFinal())
         return {};
@@ -35,29 +46,35 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
     {
         auto current = open.front();
         open.pop();
+
         if (closed.find(*current) != closed.end())
             continue;
+
         closed.insert(*current);
 
         for (auto &a : current->actions())
         {
+            CHECK_MEMORY_LIMIT(mem_limit_, SEARCH_STATE_PTR_SIZE);
             auto next = std::make_shared<SearchState>(a.execute(*current));
             if (closed.find(*next) != closed.end())
                 continue;
 
+            CHECK_MEMORY_LIMIT(mem_limit_, SEARCH_STATE_PTR_SIZE);
             open.push(next);
-            parent.insert({next, {current, a}});
-            if (next->isFinal()) {
-                //std::cerr << "current: " << getCurrentRSS() << " peak: " << getPeakRSS() << std::endl;
-                return reconstructPath(parent, next);
+            CHECK_MEMORY_LIMIT(mem_limit_, NODE_SIZE);
+            nodes.insert({next, {current, a}});
+            if (next->isFinal())
+            {
+                return reconstructPath(nodes, next, mem_limit_);
             }
         }
     }
+
+    return {};
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state)
 {
-    return {};
 }
 
 double StudentHeuristic::distanceLowerBound(const GameState &state) const
